f8dabf38e7bc3e9c9e866a56a544c399bf243e78
Gangyong92 <hyeonggang@naver.com>
2021-01-19 10:48:26 +0900

    #1.3 Redoocks ~#9.3
    context 기초

--- src/Screen.js
+++ src/Screen.js
@@ -1,14 +1,15 @@
-import React, { useContext } from "react";
-import { UserContext } from "./context";
+import React from "react";
+import { useFns } from "./context";
 import Header from "./Header";
 
 const Screen = () => {
-  const { logUserIn } = useContext(UserContext);
+  const { logUserIn, logUserOut } = useFns();
   return (
     <div>
       <Header />
       <h1>First screen</h1>
       <button onClick={logUserIn}>Log user in</button>
+      <button onClick={logUserOut}>Log user out</button>
     </div>
   );
 };
--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
-export const UserContext = React.createContext();
+const UserContext = React.createContext();
 
 const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState({
@@ -8,11 +8,22 @@ const UserContextProvider = ({ children }) => {
     loggedIn: false,
   });
   const logUserIn = () => setUser({ ...user, loggedIn: true });
+  const logUserOut = () => setUser({ ...user, loggedIn: false });
   return (
-    <UserContext.Provider value={{ user, logUserIn }}>
+    <UserContext.Provider value={{ user, fn: { logUserIn, logUserOut } }}>
       {children}
     </UserContext.Provider>
   );
+};
+
+export const useUser = () => {
+  const { user } = useContext(UserContext);
+  return user;
+};
+
+export const useFns = () => {
+  const { fn } = useContext(UserContext);
+  return fn;
 };
 
 export default UserContextProvider;
