02aa45bc2957d0330cef3d5eb65e6cde9654a582
Gangyong92 <hyeonggang@naver.com>
2021-01-19 19:14:40 +0900

    1.2 ToDos ~#9.11

---
+++ src/reducer.js
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const initialState = {
+  toDos: [],
+  completed: [],
+};
+
+export const ADD = "add";
+export const DEL = "del";
+export const COMPLETE = "complete";
+export const UNCOMPLETE = "uncomplete";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD:
+      return {
+        ...state,
+        toDos: [{ text: action.payload, id: uuidv4() }, ...state.toDos],
+      };
+    case DEL:
+      return {
+        ...state,
+        toDos: state.toDos.filter((toDo) => toDo.id !== action.payload),
+        completed: state.completed.filter((toDo) => toDo.id !== action.payload),
+      };
+    case COMPLETE:
+      const target = state.toDos.find((toDo) => toDo.id === action.payload);
+      return {
+        ...state,
+        toDos: state.toDos.filter((toDo) => toDo.id !== action.payload),
+        completed: [{ ...target }, ...state.completed],
+      };
+    case UNCOMPLETE:
+      const aTarget = state.completed.find(
+        (toDo) => toDo.id === action.payload
+      );
+      return {
+        ...state,
+        completed: state.completed.filter((toDo) => toDo.id !== action.payload),
+        toDos: [{ ...aTarget }, ...state.toDos],
+      };
+    default:
+      return;
+  }
+};
+
+export default reducer;
