8924586741bfa49059cf20b42b86a7e33d56e0cf
Gangyong92 <hyeonggang@naver.com>
2021-01-19 13:23:19 +0900

    1.0 ToDos ~#9.6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Lang from "./context";
-import translations from "./translations";
-import Screen from "./Screen";
 
 function App() {
   return (
-    <Lang defaultLang="en" translations={translations}>
-      <Screen />
-    </Lang>
+    <>
+      <h1>To Dos</h1>
+    </>
   );
 }
 
